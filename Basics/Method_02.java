b0091b6c381260b1409105f34ca4f7bb1749e4a9
Gaurav Singh <gsinghgv722@gmail.com>
2026-03-04 21:34:40 +0530

    Add show() method and call it in main method

---
+++ Basics/Method_02.java
@@ -0,0 +1,9 @@
+public class Method_02 {
+    public void show(){
+        System.out.println("Hello Everyone !");
+    }
+    public static void main(String[] args) {
+        Method_02 x = new Method_02();
+        x.show();
+    }
+}
