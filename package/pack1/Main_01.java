fe03034a6fd6dfdf0160a903c76f6c11ade26f69
Gaurav Singh <gsinghgv722@gmail.com>
2026-03-05 09:40:21 +0530

    Add Example of private access modifier.

---
+++ package/pack1/Main_01.java
@@ -0,0 +1,13 @@
+//Private access modifier.
+
+package pack1;
+
+public class Main_01 {
+    private void show(){
+        System.out.println("Hello Everyone!");
+    }
+    public static void main(String[] args) {
+        Main_01 obj = new Main_01();
+        obj.show();
+    }
+}
--- package/pack2/Test1.java
+++
@@ -1,9 +0,0 @@
-package pack2;
-import pack1.*;
-
-class C extends A{
-    public static void main(String[] args) {
-        C r = new C();
-        r.show();
-    }
-}
