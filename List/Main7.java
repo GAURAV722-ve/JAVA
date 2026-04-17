4be2ea81b088af77a3e81bca765ca5f96227513e
GAURAV722-ve <gsinghgv722@gmail.com>
2026-04-17 09:33:36 +0530

    Update list

--- List/Main7.java
+++ List/Main7.java
@@ -1,17 +1,17 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
-public class Main6 {
+public class Main7 {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
+         List<Integer> list = new ArrayList<>();
         list.add(10);
         list.add(50);
         list.add(30);
         list.add(40);
         list.add(20);
         System.out.println("Original list : "+list);
-        list.sort(Comparator.naturalOrder());
-        System.out.println("Sorted list : "+list);
+
+        list.set(2,1000);
+        System.out.println("Updated list : "+list);
     }
 }
