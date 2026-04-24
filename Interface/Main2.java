b7b92ad7107f07716464e4d2b13c6d89277fd105
GAURAV722-ve <gsinghgv722@gmail.com>
2026-04-24 13:18:32 +0530

    Interface Code

---
+++ Interface/Main2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+interface client{
+    int a = 22;
+    void input();
+    void output();
+}
+
+class A implements client{
+    String name;
+    Double sal;
+    public void input(){
+        Scanner S = new Scanner(System.in);
+        System.out.print("Enter Name : ");
+        String name = S.nextLine();
+        this.name = name;
+        System.out.print("Enter Salary : ");
+        Double sal = S.nextDouble();
+        this.sal = sal;
+    }
+
+    public void output(){
+        System.out.println("Name : "+name);
+        System.out.println("Salary : "+sal);
+    }
+}
+
+public class Main2 {
+    public static void main(String[] args) {
+        client C = new A();
+        C.input();
+        C.output();
+    }
+}
