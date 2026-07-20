c4054d010216a85ffd2dd9de089783ad5caa67c4
GAURAV722-ve <gsinghgv722@gmail.com>
2026-07-20 13:02:39 +0530

    Anonymous object

---
+++ Anonymous/anonymous_obj1.java
@@ -0,0 +1,11 @@
+class Student {
+    Student() {
+        System.out.println("Student object created");
+    }
+}
+
+public class anonymous_obj1{
+    public static void main(String[] args) {
+        new Student();   // Anonymous object
+    }
+}
